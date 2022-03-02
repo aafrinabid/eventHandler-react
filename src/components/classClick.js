823c9402ede16cfa634e6cc11b3735043630ad96
aafrinabid <mohdaafrin@gmail.com>
2022-03-02 11:56:04 +0530

    docs method

--- src/components/classClick.js
+++ src/components/classClick.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 
  class classClick extends Component {
+     constructor(props) {
+       super(props)
+     
+       this.state = {
+        message:'hello'
+       }
+       this.clickHandler=this.clickHandler.bind(this)
+     }
  clickHandler() {
-    console.log('fuck you why you did tht again just so you know its hurting me')
+  this.setState({
+      message:"bye bye dear will sure do miss you "
+  })
     
 }
 
   render() {
     return (
       <div>
-<button onClick={this.clickHandler}>click me ok</button>
+{/* <button onClick={this.clickHandler}>click me ok</button> */}
+<div>{this.state.message}</div>
+ <button onClick={this.clickHandler}>click me ok</button> 
 
 
       </div>
